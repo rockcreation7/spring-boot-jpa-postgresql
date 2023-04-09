037f6a3618c1676d1ed368f88e1628a343eb569b
Rock <rockcreation7@gmail.com>
2023-04-09 18:30:58 +0100

    Update TutorialRepository.java
    Repository spring boot 3 annotation for repository

--- src/main/java/com/bezkoder/spring/jpa/postgresql/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/jpa/postgresql/repository/TutorialRepository.java
@@ -3,9 +3,10 @@ package com.bezkoder.spring.jpa.postgresql.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.bezkoder.spring.jpa.postgresql.model.Tutorial;
 
+@Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
   List<Tutorial> findByPublished(boolean published);
 
